06ff216951e9d52267de3f9e518bc10a80332df8
RogerUFPS <rogersantiagomh@ufps.edu.co>
2025-03-17 00:17:58 -0500

    Update

--- Proyecto/src/ejercicio/Main.java
+++ Proyecto/src/ejercicio/Main.java
@@ -1,10 +1,31 @@
 package ejercicio;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Main extends Application{
+	@Override
+	public void start(Stage stage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("scene.fxml"));
+			Scene scene = new Scene(root);
+
+			stage.setTitle("Sistema Numerico");
+			stage.setResizable(false);
+			stage.setScene(scene);
+			stage.show();
+		} catch (IOException e) {
+			e.printStackTrace();
+			System.out.println("Error al cargar el archivo FXML.");
+		}
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		launch(args);
 	}
 
 }
